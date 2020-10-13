243a80a4c8a4aa282ac82bf8020a5d7ce49440b4
IDSCAN\bmulhall <bmulhall@idscan.net>
2020-10-13 14:37:51 -0500

    updating the name of the app and the README file to be more accurate

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-sample-app app is running!');
+    expect(page.getTitleText()).toEqual('dvs-sample-angular-app app is running!');
   });
 
   afterEach(async () => {
